239e86ba545ffb1753e3c89c5459ad085e850142
Cesar Lugo <cesarlugo00@hotmail.com>
2019-03-22 14:39:53 +0100

    installed bootstrap, MicroPost geters and setters

---
+++ src/Controller/MicroPostController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MicroPostRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/micro-post")
+ */
+
+class MicroPostController {
+
+   public function __construct(
+      \Twig_Environment $twig, MicroPostRepository $microPostRepository
+      ){
+      $this->twig = $twig;
+      $this->microPostRepository = $microPostRepository;
+   }
+   /**
+    * @Route("/", name="micro_post_index")
+    */
+   public function index() {
+      $html = $this->twig->render('micro-post/index.html.twig', [
+         'posts' => $this->microPostRepository->findAll()
+      ]);
+      return new Response($html);
+   }
+
+ }
