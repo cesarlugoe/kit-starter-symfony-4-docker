1b81843ab5659e865692b8ebbbdf572c33aa18dd
Cesar Lugo <cesarlugo00@hotmail.com>
2019-04-04 20:30:21 +0200

    forms

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -1,31 +1,75 @@
 <?php
-
+//estamos un poco enfrascados con un error epor el formPostRepository wiring
 namespace App\Controller;
 
+use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/micro-post")
  */
 
-class MicroPostController {
+class MicroPostController
+{
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
 
-   public function __construct(
-      \Twig_Environment $twig, MicroPostRepository $microPostRepository
-      ){
-      $this->twig = $twig;
-      $this->microPostRepository = $microPostRepository;
-   }
-   /**
-    * @Route("/", name="micro_post_index")
-    */
-   public function index() {
-      $html = $this->twig->render('micro-post/index.html.twig', [
+    /**
+     * @var MicroPostRepository
+     */
+    private $microPostRepository;
+
+   //  /**
+   //   * @var FormFactoryInterface
+   //   */
+   //  private $formFactory;
+
+    public function __construct(
+       \Twig_Environment $twig,
+       MicroPostRepository $microPostRepository
+       //FormFactoryInterface $formFactory
+      ) {
+        $this->twig = $twig;
+        $this->microPostRepository = $microPostRepository;
+        //$this->formFactory = $formFactory;
+    }
+
+    /**
+     * @Route("/", name="micro_post_index")
+     */
+    public function index()
+    {
+        $html = $this->twig->render('micropost/index.html.twig', [
          'posts' => $this->microPostRepository->findAll()
       ]);
-      return new Response($html);
-   }
+        return new Response($html);
+    }
 
- }
+    /**
+     * @Route("/add", name="micro_post_add")
+     */
+    public function add(Request $request)
+    {
+       $microPost = new MicroPost();
+       $microPost->setTime(new \DateTime());
+       
+       $form = $this->formFactory->create(MicroPostType::class, $microPost);
+       $form->handleRequest($request);
+
+       if ($form-isSubmited() && $form->isValid()) {
+
+       }
+
+       return new Response(
+          $this->twig->render('micropost/add.html.twig', 
+          ['form' => $form->createView()]
+          )
+       );
+    }
+}
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\MicroPost;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        for ($i = 0; $i < 10; $i++) {
+            $microPost = new MicroPost();
+            $microPost->setText('some random text' . rand(0, 100));
+            $microPost->setTime(new \DateTime('2019-03-26'));
+            $manager->persist($microPost);
+        }
+        $manager->flush();
+    }
+}
