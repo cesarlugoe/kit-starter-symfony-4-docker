a55e7305b53002ee5e45027e13ee1ef420960624
Cesar Lugo <cesarlugo00@hotmail.com>
2019-04-23 12:21:40 +0200

    persistance working

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,11 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\MicroPost;
+use App\Form\MicroPostType;
 use App\Repository\MicroPostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * @Route("/micro-post")
@@ -25,19 +29,23 @@ class MicroPostController
      */
     private $microPostRepository;
 
-   //  /**
-   //   * @var FormFactoryInterface
-   //   */
-   //  private $formFactory;
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
 
     public function __construct(
        \Twig_Environment $twig,
-       MicroPostRepository $microPostRepository
-       //FormFactoryInterface $formFactory
+       MicroPostRepository $microPostRepository,
+       FormFactoryInterface $formFactory, 
+       EntityManagerInterface $entityManager, 
+       RouterInterface $router
       ) {
         $this->twig = $twig;
         $this->microPostRepository = $microPostRepository;
-        //$this->formFactory = $formFactory;
+        $this->formFactory = $formFactory;
+        $this->entityManager = $entityManager;
+        $this->router = $router;
     }
 
     /**
@@ -62,8 +70,13 @@ class MicroPostController
        $form = $this->formFactory->create(MicroPostType::class, $microPost);
        $form->handleRequest($request);
 
-       if ($form-isSubmited() && $form->isValid()) {
+       if ($form->isSubmitted() && $form->isValid()) {
+         $this->entityManager->persist($microPost);
+         $this->entityManager->flush();
 
+         return new RedirectResponse(
+            $this->router->generate('micro_post_index')
+         );
        }
 
        return new Response(
